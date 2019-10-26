74c377d2d33560f28c994e7678c721f3cb47957a
unknown <senaikiz@gmail.com>
2019-10-26 15:54:02 +0300

    Site için HTML dosyasını değiştirdim.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5((r)0_yi3nmj=w%l&bzcpd)@&gd!4=int5f8n9@1t0_1g_4rs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", ".pythonanywhere.com"]
 
